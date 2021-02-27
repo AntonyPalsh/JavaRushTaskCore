592147e1c4ab47222747975a8cbbb6041747a565
Antony_Plsh <m13link@gmail.com>
2021-02-27 19:24:45 +1000

    add task 1812

--- task18/task1812/QuestionFileOutputStream.java
+++ task18/task1812/QuestionFileOutputStream.java
@@ -3,6 +3,7 @@ package com.javarush.task.task18.task1812;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 /* 
 Расширяем AmigoOutputStream
@@ -10,5 +11,37 @@ import java.io.InputStreamReader;
 
 public class QuestionFileOutputStream implements AmigoOutputStream {
 
+    private AmigoOutputStream amigoOutputStream;
+    private BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public QuestionFileOutputStream(AmigoOutputStream outputStream) {
+        this.amigoOutputStream = outputStream;
+    }
+
+    @Override
+    public void flush() throws IOException {
+        amigoOutputStream.flush();
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        amigoOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        amigoOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        amigoOutputStream.write(b, off, len);
+    }
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("Вы действительно хотите закрыть поток? Д/Н");
+        if (bufferedReader.readLine().equals("Д")) amigoOutputStream.close();
+    }
 }
 
