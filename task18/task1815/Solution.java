e70804c96d99d565c0abae5718b4cec81acc2773
Antony_Plsh <m13link@gmail.com>
2021-02-28 15:35:26 +1000

    add task 1814

--- task18/task1815/Solution.java
+++ task18/task1815/Solution.java
@@ -7,8 +7,29 @@ import java.util.List;
 */
 
 public class Solution {
-    public class TableInterfaceWrapper {
+    public class TableInterfaceWrapper implements TableInterface {
+        TableInterface tableInterface;
 
+
+        public TableInterfaceWrapper(TableInterface tableInterface) {
+            this.tableInterface = tableInterface;
+        }
+
+        @Override
+        public void setModel(List rows) {
+            System.out.println(rows.size());
+            tableInterface.setModel(rows);
+        }
+
+        @Override
+        public String getHeaderText() {
+            return tableInterface.getHeaderText().toUpperCase();
+        }
+
+        @Override
+        public void setHeaderText(String newHeaderText) {
+            tableInterface.setHeaderText(newHeaderText);
+        }
     }
 
     public interface TableInterface {
