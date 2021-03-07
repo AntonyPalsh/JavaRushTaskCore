53c6ad02facf207d6ee5bdba4efe498d2b251d09
Antony_Plsh <m13link@gmail.com>
2021-03-07 16:39:39 +1000

    add task 1911

---
+++ task19/task1911/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task19.task1911;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Locale;
+
+/* 
+Ридер обертка
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+
+        PrintStream consoleStream = System.out;
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        PrintStream stream = new PrintStream(outputStream);
+
+        System.setOut(stream);
+
+        testString.printSomething();
+
+        String result = outputStream.toString().toUpperCase(Locale.ROOT);
+
+        System.setOut(consoleStream);
+
+        System.out.println(result);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
