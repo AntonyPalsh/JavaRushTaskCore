ae15b6ed54ac8eb7cccfb6fecc4d2274523dcad5
Antony_Plsh <m13link@gmail.com>
2021-03-07 13:03:57 +1000

    add task 1910

---
+++ task19/task1910/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task19.task1910;
+
+import java.io.*;
+import java.util.ArrayList;
+
+/* 
+Пунктуация
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader file1 = new BufferedReader(new FileReader(reader.readLine()));
+        BufferedWriter file2 = new BufferedWriter(new FileWriter(reader.readLine()));
+        reader.close();
+
+        while (file1.ready())
+            file2.write(file1.readLine().replaceAll("\\p{Punct}", ""));
+
+        file1.close();
+        file2.close();
+    }
+}
