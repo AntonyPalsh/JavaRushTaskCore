2f534cd400ccff4137ae0d5d24643192406a2280
Antony_Plsh <m13link@gmail.com>
2021-03-07 17:49:46 +1000

    add task 1915

--- task19/task1915/Solution.java
+++ task19/task1915/Solution.java
@@ -9,7 +9,26 @@ import java.io.*;
 public class Solution {
     public static TestString testString = new TestString();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader.close();
+
+        PrintStream realStream = System.out;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        PrintStream myStream = new PrintStream(byteArrayOutputStream);
+        System.setOut(myStream);
+
+        testString.printSomething();
+
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+        fileOutputStream.write(byteArrayOutputStream.toByteArray());
+        fileOutputStream.close();
+
+        System.setOut(realStream);
+        System.out.println(byteArrayOutputStream.toString());
+
     }
 
     public static class TestString {
@@ -18,4 +37,3 @@ public class Solution {
         }
     }
 }
-
